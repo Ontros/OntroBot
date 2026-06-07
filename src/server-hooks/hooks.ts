d32fc08d07969d7097dbfd19b287bd34aebb9914
Ontros <ontro512@gmail.com>
2026-06-07 06:40:16 +0200

    More gnom hooks

--- src/server-hooks/hooks.ts
+++ src/server-hooks/hooks.ts
@@ -10,13 +10,21 @@ import { registerServerMessageHook } from './index';
 //     }
 // });
 
+let emojiId: string;
+let emojiStr: string;
 // H_Gnomus mirroring
 registerServerMessageHook('1483929130088005763', async (message) => {
     if (message.author.bot) return;
     const match = message.content.match(/<:H_Gnomus:(\d+)>/);
-    if (!match) return;
-    const emojiStr = match[0];
-    const emojiId = match[1];
+    if (match) {
+        emojiStr = match[0];
+        emojiId = match[1];
+    }
+    else if (["theo", "gnom"].some((str=>message.content.toLowerCase().includes(str))))
+    {
+        if (!emojiId) return;
+    }
+    else return;
     await Promise.all([
         message.react(`H_Gnomus:${emojiId}`).catch((e) => console.error('H_Gnomus react error:', e)),
         message.reply(emojiStr).catch((e) => console.error('H_Gnomus reply error:', e)),
